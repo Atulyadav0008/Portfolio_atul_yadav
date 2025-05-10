075a380f69ee199c8ca9e8e0b4fc1d8610b5c4fb
Atulyadav0008 <singhofsaidpur@gmail.com>
2025-05-10 11:21:11 +0530

    Portfolio basic code

---
+++ script.js
@@ -0,0 +1,17 @@
+// Navbar Toggle for Mobile View
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+navToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Smooth Scroll (optional but cool)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
